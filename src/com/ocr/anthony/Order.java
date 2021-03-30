b1d609565af17188c6f8d575272c882bce940528
Pierre <pierre.meignen.44@gmail.com>
2021-03-30 12:39:51 +0200

    création test Given_ChikenInStandardInput_When_MenuIsRun_Then_DisplayCorrectProcess + methode runMenu

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,5 +1,7 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
      * Display all available menus in the restaurant.
@@ -23,5 +25,12 @@ public class Order {
             case 3 -> System.out.println("Vous avez choisi comme menu : végétarien");
             default -> System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
         }
+    }
+
+    public void runMenu() {
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb); // ici on passe la variable nb récupéré du scanner  à la methode displaySelectedMenu
     }
 }
